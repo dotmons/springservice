d5d3372c27033322d7f2157de55da2d166a6a803
dotmons <dotmons@gmail.com>
2025-03-27 15:14:08 -0400

    Fix docker issue with postgres to call a single instance of database connection

---
+++ customer/src/test/java/com/dotmonsservice/customer/AuthenticationServiceTest.java
@@ -0,0 +1,17 @@
+//package com.dotmonsservice.customer;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.TestPropertySource;
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//
+//@SpringBootTest
+////@TestPropertySource(properties = {"customer.saltValue=mockSaltValue"})
+//class AuthenticationServiceTest {
+//
+//
+//    @Test
+//    void test(){
+//        //assertEquals("mockEncodedPassword", "mockEncodedPassword");
+//    }
+//}
